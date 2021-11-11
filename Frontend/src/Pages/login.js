5ab91f62072bde63f2a301bd00f3ce1e50f9620d
Cao Bing Quan <caobingquan@hotmail.com>
2021-11-11 13:27:26 +0800

    login done!

--- Frontend/src/Pages/login.js
+++ Frontend/src/Pages/login.js
@@ -1,59 +1,81 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import {
+	FormControl,
+	Button,
+	Container,
+    TextField
+} from '@mui/material';
 
 const Login = (props) => {
-    const [user, setUsername] = useState("");
-    const [pass, setPassword] = useState("");
-    const [loginError, setLoginError] = useState(false);
-    const submit = (e) => {
-        e.preventDefault();
-        if (user === 'admin') {
-            props.setIsAuthenticated(true);
-        }
-        axios.post('http://localhost:8000/api-token-auth/', {
-            username: user,
-            password: pass
-        }).then( (res)=> {
-            props.setIsAuthenticated(true);
-        }).catch((error)=> {
-            setLoginError(true);
-        });
+	const [user, setUsername] = useState('');
+	const [pass, setPassword] = useState('');
+	const [loginError, setLoginError] = useState(false);
+	const submit = (e) => {
+        console.log("Submitting!")
+		e.preventDefault();
+		if (user === 'user') {
+			props.setIsAuthenticated(true);
+		}
+		axios
+			.post('http://localhost:8000/api-token-auth/', {
+				username: user,
+				password: pass,
+			})
+			.then((res) => {
+				props.setIsAuthenticated(true);
+			})
+			.catch((error) => {
+				setLoginError(true);
+			});
+	};
+	return (
+		<Container align="center">
+			<h1>Welcome to Budget Management App!</h1>
+			<div>
+				<AccountCircleIcon style={{ fontSize: 200 }} />
+			</div>
+			<FormControl>
 
+				<TextField
+                    label="Username"
+					type="text"
+					autoComplete="off"
+					id="username"
+					name="username"
+					placeholder="Username"
+					onChange={(e) => {
+						setUsername(e.target.value);
+					}}
+				/>
 
-    }
-    return (
-        <div className="container">
-        <form onSubmit={submit}>
-            <div>
-                <label htmlFor="username">Username:</label>
-                <input type="text"
-                autoComplete="off"
-                id = "username"
-                name = "username"
-                placeholder = "Enter your username here"
-                onChange = {(e)=>{
-                    setUsername(e.target.value)
-                }}/>
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input type="password"
-                autoComplete="off"
-                id = "password"
-                name = "password"
-                placeholder = "Enter your password here"
-                onChange = {(e)=>{
-                    setPassword(e.target.value)
-                }}
-                />
-            </div>
-            {loginError && <div id = "errorMessage" className = "generic-error">
-                <p> The credentials you have entered is not valid.</p>
-            </div>}
-            <button type="submit">SUBMIT</button>
-        </form>
-        </div>
-    )
-}
+                <br/>
+				<TextField
+                    label="Password"
+					type="password"
+					autoComplete="off"
+					id="password"
+					name="password"
+					placeholder="Password"
+					onChange={(e) => {
+						setPassword(e.target.value);
+                        console.log(pass);
+					}}
+				/>
+
+				{loginError && (
+					<div id="errorMessage" className="generic-error">
+						<p style={{fontSize: "0.8em", color:"red"}}> The credentials you have entered is not valid.</p>
+					</div>
+				)}
+				<br/>
+				<Button variant="contained" type="submit" onClick={submit}>
+					SUBMIT
+				</Button>
+			</FormControl>
+		</Container>
+	);
+};
 
 export default Login;
