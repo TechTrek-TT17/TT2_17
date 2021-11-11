23d7fe74ff8ef2e89fc41eeb657bb1a14226d9a5
trevor-onloop <trevordino@gmail.com>
2021-11-11 12:30:29 +0800

    Update hello.py

--- Backend/flaskr/hello.py
+++ Backend/flaskr/hello.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from expense import *
+
 
 app = Flask(__name__)
 
@@ -6,3 +8,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+
+# Expense Endpoints
+@app.route('/expense/update/<id>', methods=['PUT'])
+def update_expense(id):
+    return update_expense_by_id(id)
+
+
+@app.route('/expense/delete/<id>', methods=['DELETE'])
+def delete_expense(id):
+    return delete_expense_by_id(id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
