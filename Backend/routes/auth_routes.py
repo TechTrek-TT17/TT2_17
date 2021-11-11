bf5ca141f00cd43dc46d29281c5a0bfee6e4fa56
sufiyan <e0318772@u.nus.edu.sg>
2021-11-11 12:50:13 +0800

    Organized the files

---
+++ Backend/routes/auth_routes.py
@@ -0,0 +1,53 @@
+from flask import request
+from flask import Blueprint, session, request
+
+from forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from models import db
+
+auth_routes = Blueprint('auth', __name__)
+
+
+@auth_routes.route('/')
+def authenticate():
+    """
+    Authenticates a user.
+    """
+    if current_user.is_authenticated:
+        return {'200'}
+    return {'errors': ['Unauthorized']}, 401
+
+
+@auth_routes.route('/login', methods=['POST'])
+def login():
+    """
+    Logs a user in
+    """
+    form = LoginForm()
+    print(request.get_json())
+    # Get the csrf_token from the request cookie and put it into the
+    # form manually to validate_on_submit can be used
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        user = db.query.filter(username == form.data['username']).first()
+        login_user(user)
+
+        return {'login successful'}
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+
+@auth_routes.route('/logout')
+def logout():
+    """
+    Logs a user out
+    """
+    logout_user()
+    return {'message': 'User logged out'}
+
+
+@auth_routes.route('/unauthorized')
+def unauthorized():
+    """
+    Returns unauthorized JSON when flask-login authentication fails
+    """
+    return {'errors': ['Unauthorized']}, 401
---
+++ Backend/routes/expense.py
@@ -0,0 +1,55 @@
+from flask import Flask, json, request, Response
+from db import connect_to_db
+import datetime
+
+# Function to update expense
+# data needed: id, expense_name, description, amount, user_name
+
+
+def update_expense_by_id(id):
+    success = False
+    data = request.get_json()
+    print(data)
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+
+        cur.execute("UPDATE expense SET name= ?, description= ?, amount=?, updated_by=?, updated_at=? WHERE id=?",
+                    (data["name"], data["description"], data["amount"], data["updated_by"], datetime.datetime.now(),
+                     id,))
+        conn.commit()
+        success = True
+    except Exception as e:
+        print(e)
+        conn.rollback()
+    finally:
+        conn.close()
+
+    if (success):
+        return Response(json.dumps({"status": "success"}), mimetype="application/json", status=200)
+    else:
+        return Response(json.dumps({"status": "error"}), mimetype="application/json", status=500)
+
+# Function to delete expense
+# data needed: id
+
+
+def delete_expense_by_id(id):
+    success = False
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+
+        cur.execute("DELETE FROM expense WHERE id=?", (id,))
+        conn.commit()
+        success = True
+    except Exception as e:
+        print(e)
+        conn.rollback()
+    finally:
+        conn.close()
+
+    if (success):
+        return Response(json.dumps({"status": "success"}), mimetype="application/json", status=200)
+    else:
+        return Response(json.dumps({"status": "error"}), mimetype="application/json", status=500)
