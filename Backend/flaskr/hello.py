17493a2d38558ad23a1780f65ca9917c3952c336
sufiyan <e0318772@u.nus.edu.sg>
2021-11-11 15:08:33 +0800

    added authentication

--- Backend/flaskr/hello.py
+++ Backend/flaskr/hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager,  login_user, logout_user
 from expense import *
 from projects import *
 
@@ -27,6 +28,42 @@ def update_expense(id):
 @app.route('/expense/delete/<id>', methods=['DELETE'])
 def delete_expense(id):
     return delete_expense_by_id(id)
+
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    success = False
+    data = request.get_json()
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+
+        user = cur.execute("Select user from db where username= ? and password= ?",
+                           data["username"], data["password"])
+        conn.commit()
+        success = True
+    except Exception as e:
+        print(e)
+        conn.rollback()
+    finally:
+        conn.close()
+
+    if (success):
+        login_user(user)
+        return Response(json.dumps({"status": "Login success"}), mimetype="application/json", status=200)
+
+    else:
+        return Response(json.dumps({"status": " Login error"}), mimetype="application/json", status=500)
+
+
+@app.route('/logout')
+def logout():
+    """
+    Logs a user out
+    """
+    logout_user()
+    return {'message': 'User logged out'}
 
 
 if __name__ == '__main__':
--- Backend/flaskr/projects.py
+++ Backend/flaskr/projects.py
@@ -2,6 +2,7 @@ from flask import Flask, json, request, Response
 from db import connect_to_db
 import datetime
 
+
 # Function to get all entries from projects table
 def get_all_projects():
     success = False
@@ -25,5 +26,29 @@ def get_all_projects():
                           "name": aPropect[2], "description": aPropect[3], "budget": aPropect[4]})
 
         return Response(json.dumps({"status": "success", "data": result}), mimetype="application/json", status=200)
+    else:
+        return Response(json.dumps({"status": "error"}), mimetype="application/json", status=500)
+
+
+def get_all_project_by_id(id):
+    success = False
+    data = request.get_json()
+    print(data)
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+
+        projects = cur.execute("SELECT * from project where user.id = ?", id)
+        conn.commit()
+        success = True
+    except Exception as e:
+        print(e)
+        conn.rollback()
+    finally:
+        conn.close()
+
+    if (success):
+
+        return Response(json.dumps(projects), mimetype="application/json", status=200)
     else:
         return Response(json.dumps({"status": "error"}), mimetype="application/json", status=500)
